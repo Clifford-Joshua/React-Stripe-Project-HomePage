095e4b698565d0bcc19f4406be0b21b68e152c0b
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2025-05-16 16:36:15 +0100

    mobile navbar done

--- src/Components/Links/Developers.jsx
+++ src/Components/Links/Developers.jsx
@@ -1,8 +1,134 @@
 import React from "react";
 import styled from "styled-components";
+import { IoCopy } from "react-icons/io5";
+import { toast } from "react-toastify";
+import { AiFillSlackCircle } from "react-icons/ai";
+import { FiActivity, FiAlignJustify } from "react-icons/fi";
+import { FaBookOpen } from "react-icons/fa6";
 
 const Developers = () => {
-  return <Wrapper className="p-[1rem] pt-[0.5rem]">Developers</Wrapper>;
+  return (
+    <Wrapper className="pb-[4rem]  bg-slate-50 ">
+      <div>
+        {/* ====================================================== */}
+        {/* By Stage */}
+        <div
+          className="text-[1.05rem] flex gap-[0.5rem]  p-[1rem] bg-white group "
+          onClick={() => toast.error(`Sorry link isn't functional`)}
+        >
+          <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem] ">
+            <FaBookOpen />
+          </div>
+
+          {/* ====================================================== */}
+          {/* Documentation */}
+          <div className="flex flex-col gap-[0.6rem]">
+            <h2 className="capitalize font-bold">Documentation</h2>
+
+            {/* ====================================================== */}
+            {/* Get Started */}
+            <div className="flex flex-col gap-[0.8rem]">
+              <h2 className="uppercase text-[0.9rem] font-[600] text-gray-800">
+                Get started
+              </h2>
+
+              <p className="text-[0.98rem] text-gray-600 font-[500] hover:text-gray-950">
+                Prebuilt checkout
+              </p>
+
+              <p className="text-[0.98rem] text-gray-600 font-[500] hover:text-gray-950">
+                Libraries and SDKs
+              </p>
+
+              <p className="text-[0.98rem] text-gray-600 font-[500] hover:text-gray-950">
+                App integrations
+              </p>
+            </div>
+
+            {/* ====================================================== */}
+            {/* Guides */}
+            <div className="flex flex-col gap-[0.8rem]">
+              <h2 className="uppercase text-[0.9rem] font-[600] pt-[0.6rem] text-gray-800">
+                Guides
+              </h2>
+
+              <p className="text-[0.98rem] text-gray-600 font-[500] hover:text-gray-950">
+                Accepts online payments
+              </p>
+
+              <p className="text-[0.98rem] text-gray-600 font-[500] hover:text-gray-950">
+                Manage subscriptions
+              </p>
+
+              <p className="text-[0.98rem] text-gray-600 font-[500] hover:text-gray-950">
+                Send payments
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* ====================================================== */}
+        {/* slate color section */}
+        <div className=" p-[1rem] py-[2.5rem] flex flex-col gap-[1.4rem]">
+          {/* ====================================================== */}
+          {/* Full Api reference*/}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <FiAlignJustify />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Full Api reference
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/* API status */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <AiFillSlackCircle />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              API status
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/* API changelog */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <FiActivity />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              API changelog
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/*Build on stripe Apps */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <IoCopy />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Build on stripe Apps
+            </h3>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
--- src/Components/Links/Resources.jsx
+++ src/Components/Links/Resources.jsx
@@ -1,7 +1,177 @@
 import React from "react";
+import { toast } from "react-toastify";
+import {
+  IoCopy,
+  IoChatbubbles,
+  IoBagSharp,
+  IoAtCircleOutline,
+} from "react-icons/io5";
+
+import { FaPlusSquare, FaBookDead } from "react-icons/fa";
+
+import { AiFillSlackCircle } from "react-icons/ai";
+import { FiAlignJustify } from "react-icons/fi";
+import { BsNewspaper } from "react-icons/bs";
+import { HiDatabase } from "react-icons/hi";
 import styled from "styled-components";
 const Resources = () => {
-  return <Wrapper className="p-[1rem] pt-[0.5rem]">Resources</Wrapper>;
+  return (
+    <Wrapper className="pb-[4rem]  bg-slate-50 ">
+      <div>
+        {/* ====================================================== */}
+        {/* first section */}
+        <div className="bg-white p-[1rem] py-[2.5rem] flex flex-col gap-[1.8rem]">
+          {/* ====================================================== */}
+          {/*  Support center*/}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <IoAtCircleOutline />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Support center
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/*  Supports plans */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <AiFillSlackCircle />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Supports plans
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/* Guides */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <FaBookDead />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">Guides</h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/* Customer stories */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <FiAlignJustify />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Customer stories
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/*Blog */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <HiDatabase />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">Blog</h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/*Sessions */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <IoBagSharp />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">Sessions</h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/*Contact sales */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <IoChatbubbles />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Contact sales
+            </h3>
+          </div>
+        </div>
+
+        {/* ====================================================== */}
+        {/* slate color section */}
+        <div className=" p-[1rem] py-[2.5rem] flex flex-col gap-[1.8rem]">
+          {/* ====================================================== */}
+          {/* Job*/}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <IoBagSharp />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">Jobs</h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/* Newsroom */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <BsNewspaper />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">Newsroom</h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/* Stripe press */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <IoCopy />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Stripe Press
+            </h3>
+          </div>
+
+          {/* ====================================================== */}
+          {/*Become a partner */}
+          <div
+            className="text-[1.05rem] flex gap-[0.5rem] items-center group"
+            onClick={() => toast.error(`Sorry link isn't functional`)}
+          >
+            <div className="text-blue-400 group-hover:text-gray-950 text-[1.3rem]">
+              <FaPlusSquare />
+            </div>
+            <h3 className="font-bold text-[0.98rem] text-gray-800">
+              Become a partner
+            </h3>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
